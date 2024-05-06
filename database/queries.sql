4a66fe0ddc14e3497eb9fee7517d07ee5b8a26c0
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-06 20:26:23 -0300

    🎉 init(create table page and add data):

--- database/queries.sql
+++ database/queries.sql
@@ -1,5 +1,18 @@
 CREATE DATABASE new_massaschasko;
+USE new_massaschasko;
 
-CREATE TABLE pages (
-    id INT --CONTINUAR AQUI...
-)
+CREATE TABLE page (
+    id_page INT AUTO_INCREMENT,
+    nome_page VARCHAR(200) NOT NULL,
+    CONSTRAINT pk_page PRIMARY KEY (id_page)
+)ENGINE = INNODB;
+DESC page;
+
+INSERT INTO page
+    (nome_page)
+VALUES
+    ("Croissants"),
+    ("Esfihas"),
+    ("Nhoques"),
+    ("Pierogues"),
+    ("Outros Produtos");
