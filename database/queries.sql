817c7dd19fc66ad089c0a948dbf1efc089277ead
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-07 19:44:27 -0300

    ✨ feat(change table names and test new generates):

--- database/queries.sql
+++ database/queries.sql
@@ -1,18 +1,20 @@
+DROP DATABASE IF EXISTS new_massaschasko;
 CREATE DATABASE new_massaschasko;
 USE new_massaschasko;
 
-CREATE TABLE page (
-    id_page INT AUTO_INCREMENT,
-    nome_page VARCHAR(200) NOT NULL,
-    CONSTRAINT pk_page PRIMARY KEY (id_page)
+CREATE TABLE category (
+    id_category INT AUTO_INCREMENT,
+    name_category VARCHAR(200) NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (id_category)
 )ENGINE = INNODB;
-DESC page;
+DESC category;
 
-INSERT INTO page
-    (nome_page)
+INSERT INTO category
+    (name_category)
 VALUES
     ("Croissants"),
     ("Esfihas"),
     ("Nhoques"),
     ("Pierogues"),
     ("Outros Produtos");
+SELECT * FROM category;
