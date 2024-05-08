20a3d0650de8f70439c4eb99c7447727302e1b41
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-05-08 17:43:10 -0300

    🎉 init(create table weight and product, teste new generates):

--- database/queries.sql
+++ database/queries.sql
@@ -18,3 +18,31 @@ VALUES
     ("Pierogues"),
     ("Outros Produtos");
 SELECT * FROM category;
+
+CREATE TABLE weight (
+    id_weight INT AUTO_INCREMENT,
+    name_weight VARCHAR(200) NOT NULL,
+    CONSTRAINT pk_category PRIMARY KEY (id_weight)
+)ENGINE = INNODB;
+DESC weight;
+
+INSERT INTO weight
+    (name_weight)
+VALUES
+    ("350g"),
+    ("400g"),
+    ("1Kg"),
+    ("1,2Kg");
+SELECT * FROM weight;
+
+CREATE TABLE product (
+    id_product INT AUTO_INCREMENT,
+    name_product VARCHAR(200) NOT NULL,
+    id_category INT NOT NULL,
+    id_weight INT NOT NULL,
+    CONSTRAINT pk_product PRIMARY KEY (id_product),
+    CONsTRAINT fk_product_category FOREIGN KEY (id_category) REFERENCES category(id_category),
+    CONSTRAINT fk_product_weight FOREIGN KEY (id_weight) REFERENCES weight(id_weight)
+    ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE = INNODB;
+DESC product;
