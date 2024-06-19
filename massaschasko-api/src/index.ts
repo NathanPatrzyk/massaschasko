98302206a1915e658a77d4f0240522300d93a346
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-06-19 20:18:54 -0300

    ✨ feat(start with api on hono):

--- massaschasko-api/src/index.ts
+++ massaschasko-api/src/index.ts
@@ -1,6 +1,6 @@
-import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
-import { categories, pages, products } from "./db/schema";
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "./db/schema";
 
 export type Env = {
   DB: D1Database;
@@ -9,20 +9,20 @@ export type Env = {
 const app = new Hono<{ Bindings: Env }>();
 
 app.get("/pages", async (c) => {
-  const db = drizzle(c.env.DB);
-  const result = await db.select().from(pages).all();
+  const db = drizzle(c.env.DB, { schema });
+  const result = await db.query.pages.findMany();
   return c.json(result);
 });
 
-app.get("/categories", async (c) => {
-  const db = drizzle(c.env.DB);
-  const result = await db.select().from(categories).all();
+app.get("/pages/categories", async (c) => {
+  const db = drizzle(c.env.DB, { schema });
+  const result = await db.query.categories.findMany();
   return c.json(result);
 });
 
 app.get("/products", async (c) => {
-  const db = drizzle(c.env.DB);
-  const result = await db.select().from(products).all();
+  const db = drizzle(c.env.DB, { schema });
+  const result = await db.query.products.findMany();
   return c.json(result);
 });
 
