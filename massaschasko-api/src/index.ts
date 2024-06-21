766ffd68a2cd62bf80d7f234f8e5dc2df850fdcf
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-06-20 21:23:06 -0300

    ✨ feat(continue with api on hono):

--- massaschasko-api/src/index.ts
+++ massaschasko-api/src/index.ts
@@ -1,6 +1,9 @@
 import { drizzle } from "drizzle-orm/d1";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { categories, pages, products } from "./db/schema";
+import { SQLiteBaseInteger } from "drizzle-orm/sqlite-core";
+import { float } from "drizzle-orm/mysql-core";
 
 export type Env = {
   DB: D1Database;
@@ -10,19 +13,54 @@ const app = new Hono<{ Bindings: Env }>();
 
 app.get("/pages", async (c) => {
   const db = drizzle(c.env.DB);
-  const result = await db.select().from(pages).all();
+  const result = await db
+    .select({
+      id: pages.id,
+      name: pages.name,
+    })
+    .from(pages)
+    .all();
   return c.json(result);
 });
 
-app.get("/categories", async (c) => {
+app.get("/pages/categories", async (c) => {
   const db = drizzle(c.env.DB);
-  const result = await db.select().from(categories).all();
+  const result = await db
+    .select({
+      id: categories.id,
+      name: categories.name,
+      weight: categories.weight,
+      pageId: categories.pageId,
+    })
+    .from(categories)
+    .all();
   return c.json(result);
 });
 
-app.get("/products", async (c) => {
+app.get("/pages/:page_id/categories", async (c) => {
+  const pageId = Number(c.req.param("page_id"));
+
   const db = drizzle(c.env.DB);
-  const result = await db.select().from(products).all();
+  const result = await db
+    .select({
+      id: categories.id,
+      name: categories.name,
+      weight: categories.weight,
+      pageId: categories.pageId,
+    })
+    .from(categories)
+    .where(eq(categories.pageId, pageId));
+  return c.json(result);
+});
+
+app.get("/pages/categories/:category_id/products", async (c) => {
+  const categoryId = Number(c.req.param("category_id"));
+
+  const db = drizzle(c.env.DB);
+  const result = await db
+    .select()
+    .from(products)
+    .where(eq(products.categoryId, categoryId));
   return c.json(result);
 });
 
