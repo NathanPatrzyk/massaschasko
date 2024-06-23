1bb3ecc7aad21cdc44533d99eb0c25e1f857b1ea
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-06-22 21:14:59 -0300

    ✨ feat(finish the api on hono):

--- massaschasko-api/src/index.ts
+++ massaschasko-api/src/index.ts
@@ -1,7 +1,5 @@
-import { drizzle } from "drizzle-orm/d1";
-import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { categories, pages, products } from "./db/schema";
+import { routes } from "./routes";
 
 export type Env = {
   DB: D1Database;
@@ -9,90 +7,6 @@ export type Env = {
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/pages", async (c) => {
-  const db = drizzle(c.env.DB);
-  const pagesList = await db
-    .select({
-      id: pages.id,
-      name: pages.name,
-    })
-    .from(pages)
-    .all();
-  return c.json(pagesList);
-});
-
-app.get("/pages/categories", async (c) => {
-  const db = drizzle(c.env.DB);
-  const categoriesList = await db
-    .select({
-      id: categories.id,
-      name: categories.name,
-      weight: categories.weight,
-      pageId: categories.pageId,
-    })
-    .from(categories)
-    .all();
-  return c.json(categoriesList);
-});
-
-app.get("/pages/:page_id/categories", async (c) => {
-  const pageId = Number(c.req.param("page_id"));
-
-  const db = drizzle(c.env.DB);
-  const categoriesList = await db
-    .select({
-      id: categories.id,
-      name: categories.name,
-      weight: categories.weight,
-      pageId: categories.pageId,
-    })
-    .from(categories)
-    .where(eq(categories.pageId, pageId));
-  return c.json(categoriesList);
-});
-
-app.get("/pages/categories/:category_id/products", async (c) => {
-  const categoryId = Number(c.req.param("category_id"));
-
-  const db = drizzle(c.env.DB);
-  const productsList = await db
-    .select()
-    .from(products)
-    .where(eq(products.categoryId, categoryId));
-
-  const categoriesList = await db
-    .select({
-      weight: categories.weight,
-    })
-    .from(categories)
-    .where(eq(categories.id, categoryId))
-    .limit(1);
-
-  const categoryWeight = categoriesList;
-
-  const productsData: any[] = [];
-
-  productsList.map(async (product) => {
-    const productName: string = product.name;
-
-    const imageName: string = generateImageName(productName, categoryWeight);
-
-    productsData.push({ imageName });
-  });
-
-  return c.json(data);
-});
-
-const generateImageName = (name: string, weight: string) => {
-  return (
-    name
-      .toLowerCase()
-      .replace(/[^\w ]+/g, "")
-      .replace(/ +/g, "-") +
-    "-" +
-    weight +
-    ".webp"
-  );
-};
+routes(app);
 
 export default app;
---
+++ massaschasko-api/src/routes.ts
@@ -0,0 +1,112 @@
+import { drizzle } from "drizzle-orm/d1";
+import { eq } from "drizzle-orm";
+import { Hono } from "hono";
+import { categories, pages, products } from "./db/schema";
+import {
+  generateSlug,
+  generateImageName,
+  generateDescription,
+  generateMessageForWhatsapp,
+} from "./utils";
+
+export const routes = (app: Hono<{ Bindings: { DB: D1Database } }>) => {
+  app.get("/pages", async (c) => {
+    const db = drizzle(c.env.DB);
+
+    const queriedPages = await db
+      .select({
+        id: pages.id,
+        name: pages.name,
+      })
+      .from(pages)
+      .all();
+
+    const processedPages: any[] = [];
+
+    queriedPages.map((queriedPage) => {
+      const slug: string = generateSlug(queriedPage.name);
+      processedPages.push({ ...queriedPage, slug: slug });
+    });
+
+    return c.json(processedPages);
+  });
+
+  app.get("/pages/categories", async (c) => {
+    const db = drizzle(c.env.DB);
+
+    const queriedCategories = await db
+      .select({
+        id: categories.id,
+        name: categories.name,
+        weight: categories.weight,
+        pageId: categories.pageId,
+      })
+      .from(categories)
+      .all();
+
+    return c.json(queriedCategories);
+  });
+
+  app.get("/pages/:page_id/categories", async (c) => {
+    const pageId = Number(c.req.param("page_id"));
+
+    const db = drizzle(c.env.DB);
+
+    const queriedCategories = await db
+      .select({
+        id: categories.id,
+        name: categories.name,
+        weight: categories.weight,
+        pageId: categories.pageId,
+      })
+      .from(categories)
+      .where(eq(categories.pageId, pageId));
+
+    return c.json(queriedCategories);
+  });
+
+  app.get("/pages/categories/:category_id/products", async (c) => {
+    const categoryId = Number(c.req.param("category_id"));
+
+    const db = drizzle(c.env.DB);
+
+    const queriedProducts = await db
+      .select()
+      .from(products)
+      .where(eq(products.categoryId, categoryId));
+
+    const queriedCategories = await db
+      .select({
+        weight: categories.weight,
+      })
+      .from(categories)
+      .where(eq(categories.id, categoryId))
+      .limit(1);
+
+    const { weight } = queriedCategories[0];
+
+    const processedProducts: any[] = [];
+
+    queriedProducts.map(async (queriedProduct) => {
+      const name: string = queriedProduct.name;
+
+      const imageName: string = generateImageName(name, weight);
+
+      const description: string = generateDescription(weight);
+
+      const messageForWhatsapp: string = generateMessageForWhatsapp(
+        name,
+        weight
+      );
+
+      processedProducts.push({
+        imageName,
+        name,
+        description,
+        messageForWhatsapp,
+      });
+    });
+
+    return c.json(processedProducts);
+  });
+};
