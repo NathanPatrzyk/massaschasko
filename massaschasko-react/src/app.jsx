01a0c60f00f4365910f9624425187e685b57d72e
NathanPatrzyk <59621709+NathanPatrzyk@users.noreply.github.com>
2024-09-21 16:15:00 -0300

    Update app.jsx

--- massaschasko-react/src/app.jsx
+++ massaschasko-react/src/app.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-import { Home } from "./pages/home/index.jsx";
-import { About } from "./pages/about/index.jsx";
-import { Products } from "./pages/products/index.jsx";
+import { Home } from "./pages/home";
+import { About } from "./pages/about";
+import { Products } from "./pages/products";
 
-import { Nav } from "./components/nav.jsx";
-import { Footer } from "./components/footer.jsx";
+import { Nav } from "./components/nav";
+import { Footer } from "./components/footer";
 
-import { useFetch } from "./hooks/useFetch.jsx";
+import { useFetch } from "./hooks/useFetch";
 
 export function App() {
   const url = "https://massaschasko-api.nathanpatrzyk11.workers.dev";
