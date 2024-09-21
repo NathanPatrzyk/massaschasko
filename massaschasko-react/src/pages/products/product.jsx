d616661a4e28b59e5f80d41253b222a2736e56eb
NathanPatrzyk <nathanpatrzyk11@gmail.com>
2024-09-20 21:52:41 -0300

    🔧 build(fix errors):

--- massaschasko-react/src/pages/products/product.jsx
+++ massaschasko-react/src/pages/products/product.jsx
@@ -4,7 +4,7 @@ import { Button } from "../../components/button";
 import { useFetch } from "../../hooks/useFetch";
 
 export function Product({ category }) {
-  const url = "massaschasko-api.nathanpatrzyk11.workers.dev";
+  const url = "https://massaschasko-api.nathanpatrzyk11.workers.dev";
   const productsUrl = category
     ? `${url}/pages/categories/${category.id}/products`
     : null;
